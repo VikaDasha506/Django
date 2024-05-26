b813a37804d2b5a2e272d36f2f733927a116e1cf
VikaDasha506 <Manin_lt29@top-academy.ru>
2024-05-26 16:22:35 +0300

    Пользователи.Восстановление пароля через email

--- anki/settings.py
+++ anki/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 from django.conf.global_settings import AUTH_USER_MODEL, AUTHENTICATION_BACKENDS
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv()  # Загружает переменные окружения из файла .env, который находится в той же директории, что и settings.py
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
