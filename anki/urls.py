f89ada695a3db201774e60202c0d845c76857229
VikaDasha506 <Manin_lt29@top-academy.ru>
2024-05-28 20:27:15 +0300

    Cards_clone

--- anki/urls.py
+++ anki/urls.py
@@ -22,15 +22,14 @@ from django.views.decorators.cache import cache_page
 
 admin.site.site_header = 'Управление моим сайтом'  # Текст в шапке
 admin.site.site_title = 'Административный сайт'  # Текст в титле
-admin.site.index_title = 'Добро пожаловать в панель управления' # Текст на главной странице
-
+admin.site.index_title = 'Добро пожаловать в панель управления'  # Текст на главной странице
 
 urlpatterns = [
     # Админка
     path('admin/', admin.site.urls),
     # Маршруты для меню
-    path('', cache_page(60*15)(views.IndexView.as_view()), name='index'),
-    path('about/', cache_page(60*15)(views.AboutView.as_view()), name='about'),
+    path('', cache_page(60 * 15)(views.IndexView.as_view()), name='index'),
+    path('about/', cache_page(60 * 15)(views.AboutView.as_view()), name='about'),
     # Маршруты подключенные из приложения cards
     path('cards/', include('cards.urls')),
     path('users/', include('users.urls', namespace='users')),
